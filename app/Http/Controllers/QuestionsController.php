11192488f531241844e5ca9552aa36614aea09b1
Kamran <kamran@nethawk.com.pk>
2019-10-24 23:03:14 +0500

    stored question with form validation and flash messages

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\QuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -35,9 +36,10 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(QuestionRequest $request)
     {
-        //
+      $request->user()->questions()->create($request->only('title','body'));
+      return redirect()->route('questions.index')->with('success','Question submitted successfully');
     }
 
     /**
