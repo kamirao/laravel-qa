43d42f338af0be8a33b7ccf9ab24c2b521d42fd7
Kamran <kamran@nethawk.com.pk>
2019-11-18 19:35:38 +0500

    generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -9,12 +9,20 @@ class Answer extends Model {
   public function question() {
     return $this->belongsTo(Question::class);
   }
-  
+
   public function user() {
     return $this->belongsTo(User::class);
   }
 
   public function getBodyHtmlAttribute() {
     return \Parsedown::instance()->text($this->body);
+  }
+
+  public static function boot() {
+    parent::boot();
+
+    static::created(function($answer) {
+      $answer->question->increment('answers_count');
+    });
   }
 }
