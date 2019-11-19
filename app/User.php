b75ae24ca22c34b5b50ea040d6df6793ea0f81ca
Kamran <kamran@nethawk.com.pk>
2019-11-19 19:40:23 +0500

    display answers for questions

--- app/User.php
+++ app/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
       //return route("questions.show", $this->id);
       return '#';
     }
+
+    public function getAvatarAttribute() {
+      $email = $this->email;
+      $size = 32;
+
+      return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+    }
 }
