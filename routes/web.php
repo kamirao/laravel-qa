c9219418aaed8497781a4c29606e28b6abc08c65
Kamran <kamran@nethawk.com.pk>
2019-12-25 23:48:02 +0500

    make the favorite button work

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('questions/{slug}', 'QuestionsController@show')->name('questions.show
 //Route::post('questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::resource('questions.answer', 'AnswersController')->only(['store','edit','update','destroy']);
 Route::post('/answers/{answer}/accept', 'AcceptAnswerController@__invoke')->name('answers.accept');
+
+Route::post('questions/{question}/favorites', 'FavoritesController@store')->name('questions.favorite');
+Route::delete('questions/{question}/favorites', 'FavoritesController@destroy')->name('questions.unfavorite');
